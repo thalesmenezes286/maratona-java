64bbb89a6dc3173201eb0ceaf5d362e5e17a170a
Thales R. Menezes <thalesmenezes286@gmail.com>
2024-07-09 09:16:31 -0300

    Introdução Java - Olá mundo

---
+++ src/com/maratona/dev/introducao/OlaMundo.java
@@ -0,0 +1,12 @@
+package com.maratona.dev.introducao;
+
+public class OlaMundo {
+
+    public static String hello(){
+        return "Olá mundo no Java";
+    }
+    public static void main(String[] args) {
+        System.out.println(hello());;
+    }
+}
+á
